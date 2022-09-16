fc12487488713c6ec754839acf1c0ac7e1844a11
SamiAyobi <105252940+SamiAyobi@users.noreply.github.com>
2022-09-16 11:35:58 -0700

    this is a changes from the remote
    Changes from remote

--- src/gitHub2ndOne.java
+++ src/gitHub2ndOne.java
@@ -3,6 +3,11 @@ public class gitHub2ndOne {
 
 	public static void main(String[] args) {
 		
+	//this is a new changes in from the remote repository
+		
+		int a=20;
+		int b =30;
+		system.out.prinln(a+b);
 		
 
 }
