024db86dee7c5bc27bab88681aeeacbe5e5333fb
unknown <sami.prt@gmail.com>
2022-09-16 11:16:55 -0700

    just added some more lines

--- src/GitHubPractice1234.java
+++ src/GitHubPractice1234.java
@@ -6,8 +6,9 @@ public class GitHubPractice1234 {
 		
 		String name="SAMI";
 		int age=44;
+		String hight= "5ft 10inches";
 		
-		System.out.println("My name is :" + name + " I am : "+ age+ " years old");
+		System.out.println("My name is :" + name + " I am : "+ age+ " years old" + hight);
 		
 ///this is the second line that i would like to add in the repo
 		// this is the third times changes in my local repo and i would push it in to remote repo
