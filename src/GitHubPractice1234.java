714140c5625ff7bd2ca47eceefecd00e6a8ac97e
unknown <sami.prt@gmail.com>
2022-09-16 11:06:15 -0700

    drop some comments in the class

--- src/GitHubPractice1234.java
+++ src/GitHubPractice1234.java
@@ -10,6 +10,7 @@ public class GitHubPractice1234 {
 		System.out.println("My name is :" + name + " I am : "+ age+ " years old");
 		
 ///this is the second line that i would like to add in the repo
+		// this is the third times changes in my local repo and i would push it in to remote repo
 	}
 
 }
