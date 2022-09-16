0c1df053307f0da1dae98c6d67b3b6d4c6f4e541
unknown <sami.prt@gmail.com>
2022-09-16 10:29:56 -0700

    the first line of code is my name and age

---
+++ src/GitHubPractice1234.java
@@ -0,0 +1,15 @@
+
+public class GitHubPractice1234 {
+
+	public static void main(String[] args) {
+	
+		
+		String name="SAMI";
+		int age=44;
+		
+		System.out.println("My name is :" + name + " I am : "+ age+ " years old");
+		
+
+	}
+
+}
