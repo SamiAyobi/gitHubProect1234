de6c023474a24b6ea6b8cfa2a3ef748e2dad1483
unknown <sami.prt@gmail.com>
2022-09-16 11:21:19 -0700

    just added id

--- src/GitHubPractice1234.java
+++ src/GitHubPractice1234.java
@@ -6,9 +6,10 @@ public class GitHubPractice1234 {
 		
 		String name="SAMI";
 		int age=44;
-		String hight= "5ft 10inches";
+		int id =00112;
 		
-		System.out.println("My name is :" + name + " I am : "+ age+ " years old" + hight);
+		
+		System.out.println("My name is :" + name + " I am : "+ age+ " years old" + id);
 		
 ///this is the second line that i would like to add in the repo
 		// this is the third times changes in my local repo and i would push it in to remote repo
